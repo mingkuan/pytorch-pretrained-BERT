31bacb16d83a5c1c50740133975489e394f685e8
Mingkuan Liu <mingkliu@ebay.com>
2018-12-16 18:25:53 -0800

    Add squad test

--- examples/runExp.sh
+++ examples/runExp.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
-export GLUE_DIR=../GLUE/glue_data
+#
+## Test MRPC classifiler
+#export GLUE_DIR=../GLUE/glue_data
+#python run_classifier.py \
+#  --task_name MRPC \
+#  --do_train \
+#  --do_eval \
+#  --do_lower_case \
+#  --data_dir $GLUE_DIR/MRPC/ \
+#  --bert_model bert-base-uncased \
+#  --max_seq_length 128 \
+#  --train_batch_size 32 \
+#  --learning_rate 2e-5 \
+#  --num_train_epochs 3.0 \
+#  --output_dir ./mrpc_output/
+#
 
-
-# Test classifiler
-
-python run_classifier.py \
-  --task_name MRPC \
-  --do_train \
-  --do_eval \
-  --do_lower_case \
-  --data_dir $GLUE_DIR/MRPC/ \
+# Test squad task
+export SQUAD_DIR=../GLUE/SQUAD
+python run_squad.py \
   --bert_model bert-base-uncased \
-  --max_seq_length 128 \
-  --train_batch_size 32 \
-  --learning_rate 2e-5 \
-  --num_train_epochs 3.0 \
-  --output_dir ./mrpc_output/
+  --do_train \
+  --do_predict \
+  --do_lower_case \
+  --train_file $SQUAD_DIR/train-v1.1.json \
+  --predict_file $SQUAD_DIR/dev-v1.1.json \
+  --train_batch_size 12 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2.0 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir ./debug_squad
+
