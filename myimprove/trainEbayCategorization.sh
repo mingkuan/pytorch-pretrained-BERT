76ec560d692c8da9cb039d7b83c88eeae54f59b0
Mingkuan Liu <mingkliu@ebay.com>
2018-12-19 14:45:05 -0800

    Reduce max_seq_len for eBay classification task.

--- myimprove/trainEbayCategorization.sh
+++ myimprove/trainEbayCategorization.sh
@@ -10,7 +10,7 @@ python run_slClassify.py \
   --do_lower_case \
   --data_dir $DATA_DIR \
   --bert_model bert-base-uncased \
-  --max_seq_length 128 \
+  --max_seq_length 64 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 10.0 \
