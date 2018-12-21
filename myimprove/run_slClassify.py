1529cae59d16db7385fe60ed42fb82b972e3bd03
Mingkuan Liu <mingkliu@ebay.com>
2018-12-21 10:14:15 -0800

    fix output folder bug.

--- myimprove/run_slClassify.py
+++ myimprove/run_slClassify.py
@@ -472,8 +472,9 @@ def main():
     if not args.do_train and not args.do_eval:
         raise ValueError("At least one of `do_train` or `do_eval` must be True.")
 
-    if os.path.exists(args.output_dir) and os.listdir(args.output_dir):
-        raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
+    # if os.path.exists(args.output_dir) and os.listdir(args.output_dir):
+    #     raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
+
     os.makedirs(args.output_dir, exist_ok=True)
 
     task_name = args.task_name.lower()
